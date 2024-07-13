9053a97fa143b41ceab46268c0d22061a7adcef4
Ritik Prakash <prakashritik97@gmail.com>
2024-07-13 17:31:26 +0530

    Day1: Activity 3 Complete

--- Day1/variable_datatype.js
+++ Day1/variable_datatype.js
@@ -10,3 +10,23 @@ console.log(name);
 
 const isLoggedIn = false;
 console.log(isLoggedIn);
+
+// ============================== Activity 3 ==============================
+
+let emp_id = 101;
+let emp_name = "Ritik Prakash";
+let emp_present = true;
+
+let Employee = {
+  id: 101,
+  name: "Ritik",
+  email: "prakashritik97@gmail.com",
+};
+
+let emp_skill = ["html", "css", "javascript", "java", "mongodb", "My-SQL"];
+
+console.log(typeof emp_id);
+console.log(typeof emp_name);
+console.log(typeof emp_present);
+console.log(typeof Employee);
+console.log(typeof emp_skill);
