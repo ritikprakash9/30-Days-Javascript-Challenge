6cbba4576f3a944ca09fb4e2b666920066d60737
Ritik Prakash <prakashritik97@gmail.com>
2024-07-13 17:22:53 +0530

    Day1 : Activity 2 Completed

--- Day1/variable_datatype.js
+++ Day1/variable_datatype.js
@@ -1,7 +1,12 @@
-// ========= Activity 1 =========
+// ============================== Activity 1 ==============================
 
 var id = 25;
 console.log(id);
 
 let name = "Ritik";
 console.log(name);
+
+// ============================== Activity 2 ==============================
+
+const isLoggedIn = false;
+console.log(isLoggedIn);
