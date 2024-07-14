712700ee15925ee4fb83369aaf94f61887a08272
Ritik Prakash <prakashritik97@gmail.com>
2024-07-14 08:53:30 +0530

    Day2: Activity 1 Completed

---
+++ Day2/operator.js
@@ -0,0 +1,19 @@
+//================================== Activity 1 ==================================
+
+let num1 = 4;
+let num2 = 2;
+
+let add_result = num1 + num2;
+console.log(add_result);
+
+let sub_result = num1 - num2;
+console.log(sub_result);
+
+let mul_result = num1 * num2;
+console.log(mul_result);
+
+let div_result = num1 / num2;
+console.log(div_result);
+
+let rem_result = num1 % num2;
+console.log(rem_result);
