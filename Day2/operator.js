65a50a634b96a11fba48d64462bb384f48fb982d
Ritik Prakash <prakashritik97@gmail.com>
2024-07-14 09:09:44 +0530

    Day 2 : Activity 3 Complete

--- Day2/operator.js
+++ Day2/operator.js
@@ -29,3 +29,37 @@ console.log(maxSpeed);
 
 maxSpeed -= 50;
 console.log(maxSpeed);
+
+console.clear();
+
+//================================== Activity 3 ==================================
+
+let marks_in_maths = 85;
+let marks_in_computer = 90;
+
+console.log(marks_in_computer > marks_in_maths);
+console.log(marks_in_computer < marks_in_computer);
+
+marks_in_maths += 5;
+console.log(marks_in_maths <= marks_in_computer);
+console.log(marks_in_maths >= marks_in_computer);
+
+console.clear();
+
+let Ducatti = {
+  name: "Ducatti Panigale",
+  topSpeed: "299",
+};
+
+let Kawasaki = {
+  name: "Kawasaki Ninja ZX-10R",
+  topSpeed: "299",
+};
+
+console.log(Ducatti.topSpeed == Kawasaki.topSpeed);
+console.log(Ducatti.topSpeed === Kawasaki.topSpeed);
+
+/*
+    == : only check value
+    === : Check value as well as data type
+*/
