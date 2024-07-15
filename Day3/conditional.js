5eb76b5439538248bd39fe3229be75a0f1a3f1ab
Ritik Prakash <prakashritik97@gmail.com>
2024-07-15 14:45:04 +0530

    Day3: Activity 3 Complete

--- Day3/conditional.js
+++ Day3/conditional.js
@@ -21,3 +21,25 @@ if (age <= 0) {
 } else {
   console.log("You are not eligible to vote");
 }
+
+console.clear();
+
+//================================== Activity 3 ==================================
+
+let num1 = 200;
+let num2 = 35;
+let num3 = 5;
+
+if (num1 > num2) {
+  if (num1 > num3) {
+    console.log("Number 1 is Greater");
+  } else {
+    console.log("Number 3 is Greater");
+  }
+} else {
+  if (num2 > num3) {
+    console.log("Number 2 is greater");
+  } else {
+    console.log("Number 3 is greater");
+  }
+}
