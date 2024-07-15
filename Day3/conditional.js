686e91d6a721675b138dae6525e050984cf5fe52
Ritik Prakash <prakashritik97@gmail.com>
2024-07-15 14:27:12 +0530

    Day 3: Activity 2 Complete

--- Day3/conditional.js
+++ Day3/conditional.js
@@ -8,3 +8,16 @@ if (number < 0) {
 } else {
   console.log("Number is zero");
 }
+
+console.clear();
+
+//================================== Activity 2 ==================================
+
+let age = 2;
+if (age <= 0) {
+  console.log("Please enter a valid age");
+} else if (age >= 18) {
+  console.log("You are eligible to vote");
+} else {
+  console.log("You are not eligible to vote");
+}
