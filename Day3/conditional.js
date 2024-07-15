462d504e70cffb5b271e63463231a55c27e77e11
Ritik Prakash <prakashritik97@gmail.com>
2024-07-15 14:23:58 +0530

    Day 3: Activity 1 Complete

---
+++ Day3/conditional.js
@@ -0,0 +1,10 @@
+//================================== Activity 1 ==================================
+let number = 0;
+
+if (number < 0) {
+  console.log("Number is Negative");
+} else if (number > 0) {
+  console.log("Number is Positive");
+} else {
+  console.log("Number is zero");
+}
