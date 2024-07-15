6a2e0769a63a59d44de406179297c8ebc16d8287
Ritik Prakash <prakashritik97@gmail.com>
2024-07-15 15:08:15 +0530

    Day 3: Task 4 Complete

--- Day3/conditional.js
+++ Day3/conditional.js
@@ -43,3 +43,77 @@ if (num1 > num2) {
     console.log("Number 3 is greater");
   }
 }
+
+console.clear();
+//================================== Activity 4 ==================================
+
+let days = 4;
+
+switch (days) {
+  case 1:
+    console.log("Monday");
+    break;
+
+  case 2:
+    console.log("Tuesday");
+    break;
+
+  case 3:
+    console.log("Wednesday");
+    break;
+
+  case 4:
+    console.log("Thrusday");
+    break;
+
+  case 5:
+    console.log("Firday");
+    break;
+
+  case 6:
+    console.log("Saturday");
+    break;
+
+  case 7:
+    console.log("Sunday");
+    break;
+
+  default:
+    console.log("Please Enter a Valid Date");
+}
+
+let score = 29;
+
+if (score < 0 && score > 100) {
+  console.log("Invalid Marks");
+}
+
+switch (score / 10) {
+  case 10:
+  case 9:
+    console.log("A Grade");
+    break;
+
+  case 8:
+    console.log("B Grade");
+    break;
+
+  case 7:
+    console.log("C Grade");
+    break;
+
+  case 6:
+    console.log("C Grade");
+    break;
+
+  case 5:
+    console.log("D Grade");
+    break;
+
+  case 4:
+    console.log("E Grade");
+    break;
+
+  default:
+    console.log("F Grade");
+}
