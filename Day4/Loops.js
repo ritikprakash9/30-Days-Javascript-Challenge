42a15367514a9ea47f737c4ec64e9ed3622e846c
Ritik Prakash <prakashritik97@gmail.com>
2024-07-16 11:17:03 +0530

    Day 4: Activity 2 Task -1 Complete

--- Day4/Loops.js
+++ Day4/Loops.js
@@ -7,3 +7,15 @@ for (let i = 1; i <= 10; i++) {
 for (let i = 1; i <= 10; i++) {
   console.log(`5 x ${i} = ${5 * i}`);
 }
+
+console.clear();
+//================================== Activity 2 ==================================
+
+let i = 1;
+let sum = 0;
+while (i <= 10) {
+  sum = sum + i;
+  i++;
+}
+
+console.log(`Sum of first 10 number is ${sum}`);
