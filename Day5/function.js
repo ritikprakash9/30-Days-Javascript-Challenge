2c3888fbce329c7dd61ad14ed3bc58d79ca368a1
Ritik Prakash <prakashritik97@gmail.com>
2024-07-17 11:19:43 +0530

    Task 1: Conversion to arrow function

--- Day5/function.js
+++ Day5/function.js
@@ -2,13 +2,13 @@
 
 // Task 1:
 
-function evenOdd(num) {
+let evenOdd = (num) => {
   if (num % 2 == 0) {
     console.log(`${num} is even number`);
   } else {
     console.log(`${num} is odd number`);
   }
-}
+};
 
 evenOdd(812);
 evenOdd(413);
