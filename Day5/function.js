94fd1c144334f10ed02a6a0a454d867c3c39589e
Ritik Prakash <prakashritik97@gmail.com>
2024-07-17 11:14:54 +0530

    Activity 1: Task 1 Complete

--- Day5/function.js
+++ Day5/function.js
@@ -2,6 +2,17 @@
 
 // Task 1:
 
+function evenOdd(num) {
+  if (num % 2 == 0) {
+    console.log(`${num} is even number`);
+  } else {
+    console.log(`${num} is odd number`);
+  }
+}
+
+evenOdd(812);
+evenOdd(413);
+
 // Task 2:
 
 //================================== Activity 2 ==================================
