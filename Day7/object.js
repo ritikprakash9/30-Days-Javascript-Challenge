0836d1e6697bfc8d5fc0365454024d7603dff6ff
Ritik Prakash <prakashritik97@gmail.com>
2024-07-19 11:48:31 +0530

    Activity 2: Task 3 Completed

--- Day7/object.js
+++ Day7/object.js
@@ -6,18 +6,22 @@ const books = {
   author: "James Gosling",
   title: "Java Programming",
   year: 1995,
+  getBook() {
+    return `Book Name = ${this.title}, Author=${this.author}`;
+  },
 };
 
 console.log(books);
 
 // Task 2:
 
-console.log(`Book Name = ${books.name}, Author`);
+console.log(`Book Name = ${books.title}, Author = ${books.author}`);
 
 //================================== Activity 2 ==================================
 
 // Task 3:
 
+console.log(books.getBook);
 // Task 4:
 
 //================================== Activity 3 ==================================
