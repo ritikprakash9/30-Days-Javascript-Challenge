d0464da49b5b6607c61abd82b6c0026ab0fcd869
Ritik Prakash <prakashritik97@gmail.com>
2024-08-05 17:45:51 +0530

    add styling

--- Project/Weather App/index.js
+++ Project/Weather App/index.js
@@ -7,22 +7,17 @@ let loc = document.getElementById("city");
 let apiKey = "c601b69c296302d00667163d4d0480ba";
 let city = "Hyderabad";
 let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-console.log(url);
 
 function getWheatherDetails() {
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
       if (data.cod == "404") {
         alert("City not found");
       }
 
       let icon = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
       document.getElementById("img").setAttribute("src", icon);
-
-      console.log(data.weather[0].icon);
-
       loc.innerText = data.name;
       let temp = data.main.temp - 273.15;
       temp = Math.round(temp);
@@ -30,14 +25,9 @@ function getWheatherDetails() {
     });
 }
 
-getWheatherDetails();
-
 searchBtn.addEventListener("click", function () {
   let search = document.getElementById("search").value;
   city = search;
   url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-  console.log(url);
-
   getWheatherDetails();
-  console.log(search);
 });
